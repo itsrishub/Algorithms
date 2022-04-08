133416f969bf49ab71c9077fa4c474681c5e5a70
Rishub <48987167+itsrishub@users.noreply.github.com>
2022-04-08 21:43:48 +0530

    Create RaibowSearch.cpp

---
+++ RainbowSearch/RaibowSearch.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+
+using namespace std;
+
+#define size 9
+
+int RainbowSearch(int a[], int x)
+{
+    int k = 0;
+    if(size%2 != 0 && a[size/2] == x)
+    {
+        cout<<"found at: "<<size/2;
+        k=0;
+    }
+    else
+    {
+        for(int i=0;i<size/2;i++)
+        {
+            if(a[i] == x)
+            {
+                cout<<"found at: "<<i;
+                k=0;
+                break;
+            }
+            else if(a[size-1-i] == x)
+            {
+                cout<<"found at: "<<size-1-i;
+                k=0;
+                break;
+            }
+            else
+                k = 1;
+        }
+    }
+    if(k == 1)
+    cout<<"not found";
+    return 0;
+}
+
+int main()
+{
+    int a[size] = {1,2,3,4,5,6,7,8,9};
+    int x=4;
+    RainbowSearch(a, x);
+    return 0;
+}
